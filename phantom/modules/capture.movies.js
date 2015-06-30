5c224ff5d3b50a25c18828674fe4fa77127cf41d
Joseph <eu81273@gmail.com>
2015-06-30 19:28:25 +0900

    Movies 모듈 추가

---
+++ phantom/modules/capture.movies.js
@@ -0,0 +1,16 @@
+var Promise = require('bluebird');
+var vars = require('./capture.vars');
+var report = require('./capture.report');
+
+//동영상 관련 처리
+var movies = function (page) {
+
+	return new Promise(function (resolve, reject) {
+
+        vars.page.evaluate(function () {
+
+        });
+    });
+};
+
+module.exports = movies;
