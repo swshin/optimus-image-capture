d635e0a27f4e32ca206acb7144646f72c3942db9
Joseph <eu81273@gmail.com>
2015-07-17 11:05:54 +0900

    initialize의 변수 관련 설정을 vars로 이동

--- phantom/modules/capture.initialize.js
+++ phantom/modules/capture.initialize.js
@@ -9,28 +9,6 @@ var initialize = function () {
 	return new Promise(function (resolve, reject) {
 
 		try {
-			//jQuery path
-			vars.jQueryPath = vars.workingDirectory + "/node_modules/jquery/dist/jquery.min.js";
-			//캡쳐 결과물 파일 경로
-			vars.outputFilePath = vars.workingDirectory + "/phantom/output/";
-			//더미 이미지
-			vars.dummyImage.url = "data:image/gif;base64,R0lGODlhAQABAAAAACw=";
-			//지원되지 않는 동영상 이미지
-			vars.cautionImage.url = "file:///" + vars.workingDirectory + "/phantom/resources/" + "swf.jpg";
-			vars.cautionImage.width = 150;
-			vars.cautionImage.halfWidth = 0 | (vars.cautionImage.width/2);
-			vars.cautionImage.height = 150;
-			vars.cautionImage.halfHeight = 0 | (vars.cautionImage.height/2);
-			//재생 버튼 이미지
-			vars.playButtonImage.url = "file:///" + vars.workingDirectory + "/phantom/resources/" + "playbutton.png";
-			vars.playButtonImage.width = 100;
-			vars.playButtonImage.height = 100;
-			vars.playButtonImage.halfWidth = 0 | (vars.playButtonImage.width/2);
-			vars.playButtonImage.halfHeight = 0 | (vars.playButtonImage.height/2);
-			//템플릿에 포함할 인라인 CSS (PhantomJS의 read 함수는 동기 방식으로 동작)
-			vars.inlineCSS = fs.read(vars.workingDirectory + '/phantom/resources/inline.min.css');
-			//템플릿에 포함할 인라인 자바스크립트 (PhantomJS의 read 함수는 동기 방식으로 동작)
-			vars.inlineJS = fs.read(vars.workingDirectory + '/phantom/resources/inline.min.js').replace(/"/g, "'");
 			//output 폴더를 삭제 (PhantomJS의 removeTree 함수는 동기 방식으로 동작)
 			fs.removeTree(vars.outputFilePath);
 
--- phantom/modules/capture.vars.js
+++ phantom/modules/capture.vars.js
@@ -2,8 +2,6 @@ var fs = require('fs');
 var system = require('system');
 
 module.exports = {
-	//현재 디렉토리
-	workingDirectory : fs.workingDirectory,
 	//userAgent 설정
 	userAgent : 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US) AppleWebKit/534.7 (KHTML, like Gecko) Chrome/7.0.517.44 Safari/534.7',
 	//페이지 타임아웃 (ms)
@@ -17,13 +15,13 @@ module.exports = {
 	//썸네일 다운로드 예상 시간 (ms)
 	thumbnailDownloadDuration : 2000,
 	//jQuery path
-	jQueryPath : "",
+	jQueryPath : fs.workingDirectory + "/node_modules/jquery/dist/jquery.min.js",
 	//이미지 퀄리티
 	imageQuality : Number(system.args[2]) || 70,
 	//이미지 포맷
 	imageFormat : "jpeg",
 	//캡쳐 결과물 파일 경로
-	outputFilePath : "",
+	outputFilePath : fs.workingDirectory + "/phantom/output/",
 	//상품 번호
 	prdid : system.args[1],
 	//기존 MD5
@@ -33,28 +31,28 @@ module.exports = {
 	//뷰포트(캡쳐할 크기) 세로
 	viewportHeight : Number(system.args[3]) || 1000,
 	//템플릿에 포함할 인라인 CSS
-	inlineCSS : "",
+	inlineCSS : fs.read(fs.workingDirectory + '/phantom/resources/inline.min.css'),
 	//템플릿에 포함할 인라인 자바스크립트
-	inlineJS : "",
+	inlineJS : fs.read(fs.workingDirectory + '/phantom/resources/inline.min.js').replace(/"/g, "'"),
 	//더미 이미지
 	dummyImage : {
-		data : ""
+		url : "data:image/gif;base64,R0lGODlhAQABAAAAACw="
 	},
 	//지원되지 않는 동영상 이미지
 	cautionImage : {
-		width : 0,
-		halfWidth : 0,
-		height : 0,
-		halfHeight : 0,
-		data : ""
+		width : 150,
+		height : 150,
+		halfWidth : 75,
+		halfHeight : 75,
+		url : "file:///" + fs.workingDirectory + "/phantom/resources/" + "swf.jpg"
 	},
 	//재생 버튼 이미지
 	playButtonImage : {
-		width : 0,
-		halfWidth : 0,
-		height : 0,
-		halfHeight : 0,
-		data : ""
+		width : 100,
+		height : 100,
+		halfWidth : 50,
+		halfHeight : 50,
+		url : "file:///" + fs.workingDirectory + "/phantom/resources/" + "playbutton.png"
 	},
 	mediaProperties : []
 };
