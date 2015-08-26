c2b43762f12b9d2f2b5459a35851a647dea2b3cf
AHN JAE-HA <eu81273@gmail.com>
2015-08-26 13:05:44 +0900

    변경된 로직에 맞춰 테스트 코드 수정

--- test/test-optimus-image-capture.js
+++ test/test-optimus-image-capture.js
@@ -1,24 +1,37 @@
+var colors = require('colors/safe');
 var should  = require('should');
 var capture = require('../');
 
 // Definition
 capture.start.should.be.function;
 
+var captureOptions = {
+	url: 'http://www.gsshop.com/mi15/prd/prdImgDesc.gs?prdid=16233854'
+};
+
 // Execution
-capture.start(16678490, 70, 1000)
+capture.start(captureOptions)
 .then(function (result) {
-    should.exist(result);
-    result.should.have.property('path');
-    result.should.have.property('files');
-    result.should.have.property('template');
-    result.should.have.property('md5');
+	should.exist(result);
+	should.exist(result.code);
+	should.exist(result.message);
+
+	result.message.should.have.property('path');
+	result.message.should.have.property('files');
+	result.message.should.have.property('template');
+	result.message.should.have.property('md5');
+
+	console.log(colors.yellow.bold('\n> optimus-image-capture success.'));
 })
 .catch(function (error) {
-    should.exist(error);
-    error.should.have.property('errorCode');
-    error.should.have.property('errorMessage');
+	should.exist(error);
+
+	error.should.have.property('code');
+	error.should.have.property('message');
+
+	console.log(colors.yellow.bold('\n> optimus-image-capture failure.'));
 })
 .finally(function () {
-    // Done
-    console.log( '> optimus-image-capture tests complete.');
+	// Done
+	console.log(colors.cyan.bold('> optimus-image-capture tests complete.'));
 });
