7ef22cc4a487230a06b48fa9de2f4056df0ab744
AHN <eu81273@gmail.com>
2015-08-29 19:13:05 +0900

    종료시 예외 처리 추가

--- phantom/modules/capture.close.js
+++ phantom/modules/capture.close.js
@@ -1,14 +1,16 @@
 //초기화
 var close = function (vars) {
-
-	//페이지가 오픈되어 있으면,
-	if (vars && vars.page !== undefined && vars.page.hasOwnProperty('close')) {
-		//페이지를 닫아서 메모리 정리
-		vars.page.close();
+	try {
+		//페이지가 오픈되어 있으면,
+		if (vars && vars.page !== undefined && vars.page.hasOwnProperty('close')) {
+			//페이지를 닫아서 메모리 정리
+			vars.page.close();
+		}
 	}
-
-	//PhantomJS 종료
-	phantom.exit();
+	finally {
+		//PhantomJS 종료
+		phantom.exit();		
+	}
 };
 
 module.exports = close;
